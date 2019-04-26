57d1b61b3e607a3283afc80bcd280da75293f0fd
SofiaFredriksson <sofiapkf@gmail.com>
2019-04-26 10:54:22 +0100

    create controller for reflection, validation for reflection

--- app/controllers/api/v1/reflections_controller.rb
+++ app/controllers/api/v1/reflections_controller.rb
@@ -8,4 +8,19 @@ class Api::V1::ReflectionsController < ApplicationController
     @reflection = Reflection.find(params[:id])
     render json: @reflection
   end
+
+  def create
+    @reflection = Reflection.new(reflection_params)
+    if @reflection.save
+      render json: @reflection
+    else
+      render json: {error: 'Failed to create reflection'}
+    end
+  end
+
+  private
+
+  def reflection_params
+    params.require(:reflection).permit(:content, :score, :user_id, :activity_id)
+  end
 end
--- app/models/reflection.rb
+++ app/models/reflection.rb
@@ -1,4 +1,6 @@
 class Reflection < ApplicationRecord
   belongs_to :user
   belongs_to :activity
+
+  validates :content, :score, presence: true
 end
