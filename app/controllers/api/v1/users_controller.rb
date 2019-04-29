cafee65a121ef4586196c37cc2d662b61feaae41
SofiaFredriksson <sofiapkf@gmail.com>
2019-04-29 09:37:58 +0100

    create users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -8,4 +8,19 @@ class Api::V1::UsersController < ApplicationController
     @user = User.find(params[:id])
     render json: @user
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user
+    else
+      render json: {error: "Failed to create user"}
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :first_name, :last_name)
+  end
 end
